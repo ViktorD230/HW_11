08d42ea7ae147942909df3359f93e9c583a8bcbb
Viktor <Vikd230@gmail.com>
2023-09-25 09:20:39 +0300

    Create models.py

---
+++ app/models.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Column, Integer, String, Date
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class Contact(Base):
+    __tablename__ = "contacts"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    surname = Column(String, index=True)
+    email = Column(String, unique=True, index=True)
+    phone = Column(String)
+    birthday = Column(Date)
+    additional_info = Column(String)
