448d6fbd17d7ad7c27c49279281ef8e335ce2b77
Viktor <Vikd230@gmail.com>
2023-09-25 09:20:42 +0300

    Create main.py

---
+++ app/main.py
@@ -0,0 +1,49 @@
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
+from app import crud, models, schemas
+from app.config import get_settings
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from typing import List
+
+app = FastAPI()
+settings = get_settings()
+
+engine = create_engine(settings.database_url)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@app.post("/contacts/", response_model=schemas.Contact)
+def create_contact(contact: schemas.ContactCreate, db: Session = Depends(get_db)):
+    return crud.create_contact(db, contact)
+
+@app.get("/contacts/", response_model=List[schemas.Contact])
+def read_contacts(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    return crud.get_contacts(db, skip=skip, limit=limit)
+
+@app.get("/contacts/{contact_id}", response_model=schemas.Contact)
+def read_contact(contact_id: int, db: Session = Depends(get_db)):
+    db_contact = crud.get_contact(db, contact_id)
+    if db_contact is None:
+        raise HTTPException(status_code=404, detail="Contact not found")
+    return db_contact
+
+@app.put("/contacts/{contact_id}", response_model=schemas.Contact)
+def update_contact(contact_id: int, contact: schemas.ContactCreate, db: Session = Depends(get_db)):
+    db_contact = crud.get_contact(db, contact_id)
+    if db_contact is None:
+        raise HTTPException(status_code=404, detail="Contact not found")
+    return crud.update_contact(db, contact_id, contact)
+
+@app.delete("/contacts/{contact_id}", response_model=schemas.Contact)
+def delete_contact(contact_id: int, db: Session = Depends(get_db)):
+    db_contact = crud.get_contact(db, contact_id)
+    if db_contact is None:
+        raise HTTPException(status_code=404, detail="Contact not found")
+    return crud.delete_contact(db, contact_id)
