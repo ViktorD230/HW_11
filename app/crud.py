72e841cc41852fc02e80532e613014d9eced8de0
Viktor <Vikd230@gmail.com>
2023-09-25 09:20:46 +0300

    Create crud.py

---
+++ app/crud.py
@@ -0,0 +1,30 @@
+from sqlalchemy.orm import Session
+from . import models
+from . import schemas
+
+def create_contact(db: Session, contact: schemas.ContactCreate):
+    db_contact = models.Contact(**contact.dict())
+    db.add(db_contact)
+    db.commit()
+    db.refresh(db_contact)
+    return db_contact
+
+def get_contacts(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(models.Contact).offset(skip).limit(limit).all()
+
+def get_contact(db: Session, contact_id: int):
+    return db.query(models.Contact).filter(models.Contact.id == contact_id).first()
+
+def update_contact(db: Session, contact_id: int, contact: schemas.ContactCreate):
+    db_contact = db.query(models.Contact).filter(models.Contact.id == contact_id).first()
+    for key, value in contact.dict().items():
+        setattr(db_contact, key, value)
+    db.commit()
+    db.refresh(db_contact)
+    return db_contact
+
+def delete_contact(db: Session, contact_id: int):
+    db_contact = db.query(models.Contact).filter(models.Contact.id == contact_id).first()
+    db.delete(db_contact)
+    db.commit()
+    return db_contact
